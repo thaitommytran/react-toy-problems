e0953f4b8ec158591b5bc2a179917de855751dde
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-10 01:03:36 -0500

    step 3 completed

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
 
 class EvenAndOdd extends Component {
+  constructor() {
+    super();
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: ""
+    };
+  }
+
+  handleChange(value) {
+    this.setState({ userInput: value });
+  }
+
+  assignEvensAndOdds(userInput) {
+    let arr = userInput.split(",");
+    let evens = [];
+    let odds = [];
+
+    for (let number of arr) {
+      if (number % 2 === 0) {
+        evens.push(parseInt(number));
+      } else {
+        odds.push(parseInt(number));
+      }
+    }
+
+    this.setState({ evenArray: evens, oddArray: odds });
+  }
+
   render() {
-    return <p>Evens and Odds</p>;
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens And Odds</h4>
+        <input
+          className="inputLine"
+          onChange={(event) => this.handleChange(event.target.value)}
+        />
+        <button
+          className="confirmationButton"
+          onClick={(event) => this.assignEvensAndOdds(this.state.userInput)}
+        ></button>
+        <span className="resultsBox">
+          Evens: {JSON.stringify(this.state.evenArray)}
+        </span>
+        <span className="resultsBox">
+          Odds: {JSON.stringify(this.state.oddArray)}
+        </span>
+      </div>
+    );
   }
 }
 
