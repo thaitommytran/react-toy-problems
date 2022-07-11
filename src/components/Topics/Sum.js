3fa279a02204973f621bc52fd35f01ba44f45849
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-11 08:41:02 -0500

    step 7 completed

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
 
 class Sum extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number1: 0,
+      number2: 0,
+      sum: null
+    };
+  }
+
+  updateNumber1(value) {
+    this.setState({ number1: parseInt(value, 10) });
+  }
+
+  updateNumber2(value) {
+    this.setState({ number2: parseInt(value, 10) });
+  }
+
+  add(num1, num2) {
+    this.setState({ sum: num1 + num2 });
+  }
+
   render() {
-    return <p>Sum</p>;
+    return (
+      <div className="puzzleBox sumPB">
+        <h4>Sum</h4>
+        <input
+          className="inputLine"
+          onChange={(event) => this.updateNumber1(event.target.value)}
+        />
+        <input
+          className="inputLine"
+          onChange={(event) => this.updateNumber2(event.target.value)}
+        />
+        <button
+          className="confirmationButton"
+          onClick={() => this.add(this.state.number1, this.state.number2)}
+        >
+          Add
+        </button>
+        <span className="resultsBox">Sum: {this.state.sum}</span>
+      </div>
+    );
   }
 }
 
